cbef6dbe5c6ee5095ba45a232cad27e72e580217
George Lemon <georgelemon@protonmail.com>
2021-01-12 22:25:54 +0200

    Template adds
    Signed-off-by: George Lemon <georgelemon@protonmail.com>

---
+++ app/Application.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App;
+
+use Low\Kernel\Application as LowApplication;
+
+class Application extends LowApplication
+{
+    // things you may want to extend from Low Application
+}
---
+++ public/index.php
@@ -0,0 +1,5 @@
+<?php
+
+define('ROOT_PATH', __DIR__ . "/../");
+
+require __DIR__ . DIRECTORY_SEPARATOR . '../bootstrap.php';
